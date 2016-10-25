e7ee6ee4d46df3c81600153150394890b4e0bbe7
dataking <dataking@gmail.com>
2016-10-25 15:23:12 -0700

    update by site

--- move_site_excl_to_global.rb
+++ move_site_excl_to_global.rb
@@ -5,6 +5,7 @@ require 'colorize'
 require 'pp'
 require 'highline/import'
 require 'netaddr'
+require 'sqlite3'
 include Nexpose  
   
 require_relative 'Utils.rb'
@@ -17,40 +18,6 @@ host = ask('Enter the server name (host) for Nexpose: ') { |q| q.default = defau
 user = ask('Enter your username: ') { |q| q.default = default_user }
 pass = ask('Enter your password: ') { |q| q.echo = '*' }
 
-def hashize(cidr_ary)
-	tmp = Hash.new
-	cidr_ary.each do |c|
-		tmp[c.to_s] = c
-	end
-	return tmp
-end
-
-def cidrize(ipaddr_ary)
-	cidr_hsh = Hash.new
-	ipaddr_ary.each do |ex|
-		if ex.is_a?(IPRange)
-			if ex.to.nil?
-				#range = ex.from.to_s
-				lower = ex.from.to_s
-				upper = ex.from.to_s
-			else
-				#range = ex.from.to_s + "-" + ex.to.to_s
-				lower = ex.from.to_s
-				upper = ex.to.to_s
-			end
-			bits = Utils.calc_mask(lower,upper)
-			cidr = NetAddr::CIDR.create("#{ex.from}/#{bits}")
-			cidr_hsh[cidr.to_s] = cidr unless cidr_hsh[cidr.to_s]
-		else
-			raise "Unexpacted object class: #{ex.class}".red
-		end
-	end
-	one = NetAddr.merge(cidr_hsh.values.sort, :Objectify => true)
-	two = NetAddr.merge(one.sort, :Objectify => true)
-	three = NetAddr.merge(two.sort, :Objectify => true)
-	return hashize(three)
-end
-
 nsc = Connection.new(host, user, pass)
 nsc.login  
 at_exit { nsc.logout }  
@@ -59,6 +26,7 @@ excls = Array.new
 excls_cidrs = Array.new
 gexcls = GlobalSettings.load(nsc).asset_exclusions
 gexcls_cidrs = Array.new
+ex_by_site = Hash.new
 
 nsc.sites.each do |ssum|
 	site = Site.load(nsc, ssum.id)
@@ -67,22 +35,21 @@ nsc.sites.each do |ssum|
 	if site.excluded_addresses.is_a?(Array)
 		site.excluded_addresses.sort.each do |addr|
 			excls.push(addr)
+			if ex_by_site.key?(site.name)
+				ex_by_site[site.name] = ex_by_site[site.name] + "|" + addr.to_s
+			else 
+				ex_by_site[site.name] = addr.to_s
+			end
+			ex_by_site[addr.to_s] = site.name
 		end
 	else
 		raise "Unexpected object type: #{site.excluded_addresses.class}".red
 	end			
 end
 
-excls_cidrs = cidrize(excls)
-gexcls_cidrs = cidrize(gexcls)
+excls_cidrs = Utils.cidrize(excls)
+gexcls_cidrs = Utils.cidrize(gexcls)
 
-#excls_cidrs.each do |c|
-#	puts "#{c} : #{c.first} - #{c.last}"
-#end
-#puts "================================================================================"
-#gexcls_cidrs.each do |c|
-#	puts "#{c} : #{c.first} - #{c.last}"
-#end
 to_add_gcidrs = Hash.new
 to_rem_gcidrs = Hash.new
 to_rem_lcidrs = Hash.new
@@ -99,31 +66,47 @@ excls_cidrs.each do |lk,lobj|
 			puts "global object matches local exclusion (G:#{gk} L:#{lk}".blue
 			to_rem_lcidrs[lk] = lobj
 		else
-			to_add_gcidrs[gk] = lobj
+			to_add_gcidrs[lk] = lobj
 			to_rem_lcidrs[lk] = lobj
 		end
 	end
 end
 
-#pp to_add_gcidrs
+#puts "Global exclusions to be removed:"
+#to_rem_gcidrs.values.sort.each do |c|
+#	puts c.to_s.green
+#	gexcls_cidrs.delete(c.to_s)
+#end
+#puts "Global exclusions to be added:"
+#to_add_gcidrs.values.sort.each do |c|
+#	puts c.to_s.cyan
+#	gexcls_cidrs[c.to_s] = c unless gexcls_cidrs.keys.include?(c.to_s)
+#end
+#puts "Local exclusions to be removed:"
+#to_rem_lcidrs.values.sort.each do |c|
+#	puts c.to_s.magenta
+#end
+#puts "All global exclusions:"
+#gexcls_cidrs.keys.sort.each do |k|
+#	puts k.to_s.yellow.bold
+#end
 
-#exit 0
+puts "Clearing out site-specific exclusions...."
+nsc.sites.each do |ss|
+	s = Site.load(nsc, ss.id)
+	if  s.excluded_addresses.size > 0
+		puts "  #{s.name}  "
+		s.excluded_addresses = []
+		s.save(nsc)
+	end
+end
+puts "done."
 
-puts "Global exclusions to be removed:"
-to_rem_gcidrs.values.sort.each do |c|
-	puts c.to_s.green
-	gexcls_cidrs.delete(c.to_s)
-end
-puts "Global exclusions to be added:"
-to_add_gcidrs.values.sort.each do |c|
-	puts c.to_s.cyan
-	gexcls_cidrs[c.to_s] = c unless gexcls_cidrs.keys.include?(c.to_s)
-end
-puts "Local exclusions to be removed:"
-to_rem_lcidrs.values.sort.each do |c|
-	puts c.to_s.magenta
-end
-puts "All global exclusions:"
+puts "Adding exclusions to global settings...."
+settings = GlobalSettings.load(nsc)
 gexcls_cidrs.keys.sort.each do |k|
-	puts k.to_s.yellow.bold
+	settings.add_exclusion(k)
 end
+settings.save(nsc)
+puts "done."
+
