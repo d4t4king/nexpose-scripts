e0efe9c3b0e12411d6d0c9507f8362511bc0aad1
dataking <dataking@gmail.com>
2017-01-06 15:31:23 -0800

    tweaking to download scan log file and parse

--- scripts/srt-lookup.rb
+++ scripts/srt-lookup.rb
@@ -8,6 +8,8 @@ require 'highline/import'
 require 'ipaddr'
 
 require_relative '../lib/utils'
+require_relative '../lib/scanlog'
+
 default_host = 'nc1***REMOVED***'
 #default_port = 3780
 default_user = 'user'
@@ -18,7 +20,7 @@ user = ask("Enter your username to log on: ") { |q| q.default = default_user }
 pass = ask("Enter your password: ") { |q| q.echo = "*" }
 check_ip = ask("Enter the IP address to check: ") { |q| q.default = default_ip }
 
-check_iprobj = IPAddr.new(check_ip)
+check_iprobj = Nexpose::IPRange.new("#{check_ip}/32")
 
 @nsc = Nexpose::Connection.new(host, user, pass)
 @nsc.login
@@ -30,8 +32,9 @@ gexcl = Nexpose::GlobalSettings.load(@nsc).asset_exclusions
 #pp gexcl
 is_global_exclusion = false
 gexcl.each do |ipr|
+	ck_ipaddr = IPAddr.new(check_iprobj.to_s)
 	if ipr.to.nil?						# just a single IP
-		if IPAddr.new(ipr.from) == check_iprobj
+		if IPAddr.new(ipr.from) == ck_ipaddr
 			puts "#{check_ip} was found in the Global Exclusion list.".green
 			is_global_exclusion = true
 			break
@@ -39,7 +42,7 @@ gexcl.each do |ipr|
 	else								# Must be an actual range
 		bits = Utils.calc_mask(ipr.from,ipr.to)
 		cmp_iprobj = IPAddr.new("#{ipr.from}/#{bits}")
-		if cmp_iprobj === check_iprobj
+		if cmp_iprobj === ck_ipaddr
 			puts "#{check_ip} found in Global Exclusion list (range: #{ipr.from}-#{ipr.to})".green
 			is_global_exclusion = true
 			break
@@ -61,27 +64,51 @@ else
 		site_obj = Nexpose::Site.load(@nsc, as.site_id)
 		#pp site_obj
 		puts "Found #{as.status} scan for site: #{site_obj.name}"
-		puts "\tAbove scan started at #{as.start_time}"
-		puts "\tIncludes the following range(s):"
+		puts "Above scan started at #{as.start_time}"
+		puts "Includes the following range(s):"
 		addrs = site_obj.included_scan_targets[:addresses]
 		# check for specified IP in scan ranges
 		if addrs.size == 1
-			print "\t\t#{addrs[0].from}-#{addrs[0].to} "
-			bits = Utils.calc_mask(addrs[0].from, addrs[0].to)
-			ipaddr = IPAddr.new("#{addrs[0].from}/#{bits}")
+			print "#{addrs[0].from}-#{addrs[0].to} "
+			#bits = Utils.calc_mask(addrs[0].from, addrs[0].to)
+			#put s"[*] Got bits #{bits} for range #{addrs[0].from}-#{addrs[0].to}".yellow
+			#ipaddr = IPAddr.new("#{addrs[0].from}/#{bits}")
+			#pp ipaddr
 			# if IP/range in scan range, 
-			if ipaddr === check_iprobj
-				puts "(#{ipaddr.to_range})".red
+			retval = addrs[0] <=> check_iprobj
+			#print " retval == #{retval} ".yellow
+			if retval == 0
+				puts "(#{addrs[0]})".red
 				# export the scan log
 				print "Pulling scan log...."
 				#@nsc.export_scan(as.scan_id, "scanlog-#{as.scan_id}.zip")
-				@nsc.download("https://#{@nsc.host}:3780/data/scan/log?scan-id=#{as.scan_id}", "./")
+				@nsc.download("https://#{@nsc.host}:3780/data/scan/log?scan-id=#{as.scan_id}", "scanlog-#{as.scan_id}.zip")
 				puts "done."
 				# objectify scan logg
+				%x{unzip scanlog-#{as.scan_id}.zip}
+				log = ScanLog::Log.new("scan.log")
+				#reg = DateTime.new
+				reg = nil
+				#unreg = DateTime.new
+				unreg = nil
+				log.entries.each do |e|
+					if e.message =~ /#{check_ip}/
+						reg = DateTime.parse(e.datetime.to_s) if e.message =~ /Registered\./
+						#puts "#{e.datetime} :: #{e.message}"
+						unreg = DateTime.parse(e.datetime.to_s) if e.message =~ /Unregistered\./
+					end
+				end
 				# look for affected IP/range
+				reg = reg.new_offset('-08:00')
+				unreg = unreg.new_offset('-08:00')
+				puts "#{check_ip} was first touched at " + reg.to_s.green + "."
+				puts "The scan of #{check_ip} finished at " + unreg.to_s.green + ".'
+				%x{/bin/rm -f scan.log scanlog-#{as.scan_id}.zip}
 			else
 				puts "(not found)".green
 			end
+			#puts addrs[0].inspect.to_s.yellow
+			#puts check_iprobj.inspect.to_s.yellow.bold
 		else
 			puts "\tThere's more than one range....".yellow
 		end
