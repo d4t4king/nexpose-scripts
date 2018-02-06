291d054c121c725b2ed28ac589d748b63fed7b54
Charlie Heselton <cheselto@semprautilities.com>
2018-02-06 13:21:13 -0800

    listing users' info; TODO: need last login date

--- scripts/audit-users.rb
+++ scripts/audit-users.rb
@@ -5,6 +5,42 @@ require 'colorize'
 require 'nexpose'
 require 'highline/import'
 
+# returns a has of arrays containing userid (k) 
+# and an array of siteids (v) the user can access
+def pop_user_sites(conn)
+	userSites = Hash.new
+	conn.sites.each do |ss|
+		siteDetail = Nexpose::Site.load(conn, ss.id)
+		siteDetail.users.each do |su|
+			if userSites.has_key?(su[:id])
+				if !userSites[su[:id]].include?(ss.id)
+					userSites[su[:id]].push(ss.id)
+				end
+			else
+				# new user id
+				# create placeholder array
+				userSites[su[:id]] = Array.new
+			end
+		end
+	end
+	return userSites
+end				
+	
+# returns an array of site names for the specified user
+def get_user_sites(conn, userSites, userid)
+	sitesArray = Array.new
+	if userSites[userid].nil? or !userSites[userid].is_a?(Array)
+		return sitesArray
+	end
+	userSites[userid].each do |sid|
+		site = Nexpose::Site.load(conn, sid)
+		if !sitesArray.include?(site.name)
+			sitesArray.push(site.name)
+		end
+	end
+	return sitesArray
+end
+	
 default_host = 'is-vmcrbn-p01***REMOVED***'
 #default_port = 3780
 default_user = 'sv-nexposegem'
@@ -17,14 +53,15 @@ pass = ask("Enter your password: ") { |q| q.echo = "*" }
 
 @nsc = Nexpose::Connection.new(host, user, pass)
 @nsc.login
+
 at_exit { @nsc.logout }
 
+userSites = pop_user_sites(@nsc)
+#pp userSites
+#exit 0
+
+puts "ID,UserName,FullName,Email,AuthSource,IsAdmin,IsDisabled,IsLocked,SiteCount,Sites,GroupCount"
 @nsc.list_users.each do |user|
-	puts "=" * 72
-	pp user
-	puts "=" * 72
-	fquser = Nexpose::User.load(@nsc, user.id)
-	pp fquser
-	puts "=" * 72
-	break
+	mySites = get_user_sites(@nsc, userSites, user.id)
+	puts "#{user.id},#{user.name},\"#{user.full_name}\",#{user.email},#{user.auth_source},#{user.is_admin},#{user.is_disabled},#{user.is_locked},#{user.site_count},\"#{mySites.inspect}\",#{user.group_count}"
 end
