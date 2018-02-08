c900222281470360cd75a7d5c5400b2ee7d55227
Charlie Heselton <cheselto@semprautilities.com>
2018-02-07 20:53:17 -0800

    adding last logon to audit users script

--- scripts/audit-users.rb
+++ scripts/audit-users.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'pp'
-require 'colorize'
 require 'nexpose'
+require 'colorize'
 require 'highline/import'
 
 # returns a has of arrays containing userid (k) 
@@ -40,7 +40,17 @@ def get_user_sites(conn, userSites, userid)
 	end
 	return sitesArray
 end
-	
+
+def pop_user_last_logons(conn)
+	userLastLogons = Hash.new
+	myTable = Nexpose::DataTable._get_dyn_table(conn, '/data/admin/users?tableID=UserAdminSynopsis')
+	myTable.each do |row|
+		puts "#{row["User ID"]},#{row["Last Logon"]}"
+		userLastLogons[row["User ID"].to_i] = row["Last Logon"].to_i
+	end
+	return userLastLogons
+end
+
 default_host = 'is-vmcrbn-p01***REMOVED***'
 #default_port = 3780
 default_user = 'sv-nexposegem'
@@ -60,8 +70,27 @@ userSites = pop_user_sites(@nsc)
 #pp userSites
 #exit 0
 
-puts "ID,UserName,FullName,Email,AuthSource,IsAdmin,IsDisabled,IsLocked,SiteCount,Sites,GroupCount"
+#myTable = Nexpose::DataTable._get_dyn_table(@nsc, '/data/admin/users?tableID=UserAdminSynopsis')
+#pp myTable
+id2logon = pop_user_last_logons(@nsc)
+#pp id2logon
+myNow = DateTime.now.strftime("%s")
+#puts "Now: #{myNow}"
+#id2logon.each do |k,v|
+#	if v == 0
+#		puts "#{v} => nil"
+#	else
+#		puts "#{v} => #{DateTime.strptime(v.to_s[0...-3], "%s")}"
+#	end
+#end
+
+puts "ID,UserName,FullName,Email,AuthSource,IsAdmin,IsDisabled,IsLocked,SiteCount,GroupCount,LastLogon"
 @nsc.list_users.each do |user|
-	mySites = get_user_sites(@nsc, userSites, user.id)
-	puts "#{user.id},#{user.name},\"#{user.full_name}\",#{user.email},#{user.auth_source},#{user.is_admin},#{user.is_disabled},#{user.is_locked},#{user.site_count},\"#{mySites.inspect}\",#{user.group_count}"
+#	mySites = get_user_sites(@nsc, userSites, user.id)
+	if id2logon[user.id] == 0
+		puts "#{user.id},#{user.name},\"#{user.full_name}\",#{user.email},#{user.auth_source},#{user.is_admin},#{user.is_disabled},#{user.is_locked},#{user.site_count},#{user.group_count},0"
+	else
+		puts "#{user.id},#{user.name},\"#{user.full_name}\",#{user.email},#{user.auth_source},#{user.is_admin},#{user.is_disabled},#{user.is_locked},#{user.site_count},#{user.group_count},#{DateTime.strptime(id2logon[user.id].to_s[0...-3], "%s")}"
+	end
 end
+
